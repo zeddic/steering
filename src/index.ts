be25da1f8bce958e266aabaf5a24ee58da4f7ccb
Scott Bailey <scott@zeddic.com>
2019-10-12 15:26:50 -0700

    Configures prettier and tslint

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import * as Phaser from 'phaser';
 
 const config: Phaser.Types.Core.GameConfig = {
   title: 'Test Game',
+  // parent: 'canvas', // <-- readd once css is setup for index page
+  backgroundColor: '#000000',
   type: Phaser.AUTO,
   width: 1024,
   height: 768,
   scale: {
-      mode: Phaser.Scale.NONE,
-      autoCenter: Phaser.Scale.CENTER_BOTH
+    mode: Phaser.Scale.NONE,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
   },
   physics: {
     default: 'arcade',
@@ -15,8 +17,6 @@ const config: Phaser.Types.Core.GameConfig = {
       debug: true,
     },
   },
-  // parent: 'canvas', // <-- readd once css is setup for index page
-  backgroundColor: '#000000',
 };
 
 export const game = new Phaser.Game(config);
