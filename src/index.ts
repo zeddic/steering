4741c90b89b0a8c943673ea609d25ea0a44cf4b2
Scott Bailey <baileys@baileys-macbookpro3.roam.corp.google.com>
2019-10-26 10:18:01 -0700

    Starts to creatse a game object class.

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,60 @@ import * as PIXI from 'pixi.js';
 const app = new PIXI.Application();
 document.body.appendChild(app.view);
 
-app.loader.add('ship', 'assets/ship.gif').load((loader, resources) => {
-  const ship = new PIXI.Sprite(resources['ship']!.texture);
-  ship.x = app.renderer.width / 2;
-  ship.y = app.renderer.height / 2;
+const loader = app.loader.add('ship', 'assets/ship.gif');
+loader.load((loader, resources) => {
+  // app.stage.addChild(ship);
 
-  // Rotate around the center
-  ship.anchor.x = 0.5;
-  ship.anchor.y = 0.5;
-
-  app.stage.addChild(ship);
+  const ship = new Ship();
 
   app.ticker.add(() => {
-    ship.rotation += 0.01;
+    const deltaMS = app.ticker.deltaMS;
+    const delta = deltaMS / 1000;
+
+    ship.update(delta);
+
+    // ship.rotation += 1 * delta;
   });
 });
+
+interface GameObject {
+  position: Vector;
+  velocity: Vector;
+  acceleration: Vector;
+
+  update(delta: number): void;
+}
+
+class Ship implements GameObject {
+  position = new Vector(0, 0);
+  velocity = new Vector(0, 0);
+  acceleration = new Vector(0, 0);
+
+  constructor() {
+    const ship = new PIXI.Sprite(app.loader.resources['ship']!.texture);
+    ship.x = app.renderer.width / 2;
+    ship.y = app.renderer.height / 2;
+    ship.anchor.x = 0.5;
+    ship.anchor.y = 0.5;
+  }
+
+  update(delta: number): void {}
+}
+
+class Vector {
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+// let ticker = new PIXI.Ticker();
+// let renderer = PIXI.autoDetectRenderer();
+// let stage = new PIXI.Container();
+// document.body.appendChild(renderer.view);
+// ticker.add(function(time) {
+//   renderer.render(stage);
+// });
